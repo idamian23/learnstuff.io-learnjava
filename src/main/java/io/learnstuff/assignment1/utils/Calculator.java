75bbc5a0f57250797444517458b0e1b4348bd151
Ionut Damian <damian.ionut23@yahoo.ro>
2021-06-17 20:27:50 +0300

    Create Calculator.java

---
+++ src/main/java/io/learnstuff/assignment1/utils/Calculator.java
@@ -0,0 +1,51 @@
+package io.learnstuff.assignment1.utils;
+
+
+public class Calculator {
+
+    public static void addMatrices(int[][] first, int[][] second){
+        int row = first.length;
+        int column = first[0].length;
+        int[][] sum = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                sum[i][j] = first[i][j] + second[i][j];
+            }
+        }
+        System.out.println("Sum of Matrices: ");
+        Printer.printMatrix(sum);
+    }
+
+    public static void multipleMatrices(int[][]first, int[][] second){
+        int row = first.length;
+        int column = first[0].length;
+        int[][] product = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                product[i][j] = first[i][j] * second[i][j];
+            }
+        }
+
+        System.out.println("Product of Matrices: ");
+        Printer.printMatrix(product);
+    }
+
+    public static void scalarMatrices(int scalar, int[][] matrix){
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int[][] scalarResult = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                scalarResult[i][j] = scalar * matrix[i][j];
+            }
+        }
+
+        System.out.println("Scalar  of the matrix: ");
+        Printer.printMatrix(scalarResult);
+    }
+
+
+}
