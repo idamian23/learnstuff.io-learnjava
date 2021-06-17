b11a858a997db185cb0313b30110a52a954accc5
Ionut Damian <damian.ionut23@yahoo.ro>
2021-06-17 20:28:04 +0300

    Create Automobile.java

---
+++ src/main/java/io/learnstuff/assignment2/utils/Automobile.java
@@ -0,0 +1,8 @@
+package io.learnstuff.assignment2.utils;
+
+public class Automobile extends Vehicle{
+
+    public Automobile(String model, int year, int numWheels, int maxSpeed, String plateNumber) {
+        super(model, year, numWheels, maxSpeed, plateNumber);
+    }
+}
