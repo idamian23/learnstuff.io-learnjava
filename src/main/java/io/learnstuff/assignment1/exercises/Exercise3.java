dce9328483de82b30e2562f7a457dc089164da74
Ionut Damian <damian.ionut23@yahoo.ro>
2021-06-17 20:27:43 +0300

    Create Exercise3.java

---
+++ src/main/java/io/learnstuff/assignment1/exercises/Exercise3.java
@@ -0,0 +1,30 @@
+package io.learnstuff.assignment1.exercises;
+
+
+
+
+import io.learnstuff.assignment1.utils.Generator;
+import io.learnstuff.assignment1.utils.Printer;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    //Use the Random class to generate elements for a matrix for which you read the rows and
+    //columns from the keyboard
+
+    public static void main(String[] arg){
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows for the matrix: ");
+        int rows = scanner.nextInt();
+        System.out.println("Enter number of columns for the matrix: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = Generator.generateRandomMatrix(rows,columns);
+        Printer.printMatrix(matrix);
+    }
+}
